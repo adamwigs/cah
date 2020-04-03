c50b20663ca5338b7a06bed2a1bb1864434eb284
Ryan Williams <ryanaw@apple.com>
2020-04-02 23:47:52 -0700

    Tweaked prod domain name.

--- server/src/server.ts
+++ server/src/server.ts
@@ -21,7 +21,7 @@ const server = app.listen(port, () => {
 });
 
 const io = socketio(server, {
-    origins: env.NODE_ENV === 'production' ? ['cah.ninja:443'] : ['*:*']
+    origins: env.NODE_ENV === 'production' ? ['cah.raw.dev:*'] : ['*:*']
 });
 
 const games: Games = {};
